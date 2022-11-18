49593cf98abcd57d299dd7957c02b242da31416b
Mujtaba Sayyed <44434031+mujtaba07@users.noreply.github.com>
2022-11-19 01:51:58 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
                   className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
                />
                <button
-                  type="button"
+                  type="submit"
                   className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
                >
                   Work With Me
